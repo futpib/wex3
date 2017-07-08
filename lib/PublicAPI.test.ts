04b776dd22d53e82db9891ea1bd2b675e292a719
Alexandro <arvitaly@list.ru>
2017-07-08 08:48:18 +0300

    add all methods for public api

--- lib/PublicAPI.test.ts
+++ lib/PublicAPI.test.ts
@@ -3,6 +3,15 @@ import PublicAPI from "./PublicAPI";
 beforeEach(() => {
     fetch.resetMocks();
 });
+it("info", async () => {
+    const api = new PublicAPI();
+    const ret = {
+        test: "value1",
+    };
+    fetch.mockResponseOnce(JSON.stringify(ret));
+    expect(await api.info()).toEqual(ret);
+    expect(fetch.mock.calls).toEqual([["https://btc-e.com/api/3/info"]]);
+});
 it("ticker", async () => {
     const api = new PublicAPI();
     const value = { test: 1 };
@@ -10,6 +19,7 @@ it("ticker", async () => {
         btc_usd: value,
     }));
     expect(await api.ticker(Valute.BTC, Valute.USD)).toEqual(value);
+    expect(fetch.mock.calls).toEqual([["https://btc-e.com/api/3/ticker/btc_usd"]]);
 });
 it("tickers", async () => {
     const api = new PublicAPI();
@@ -23,10 +33,25 @@ it("tickers", async () => {
         from: Valute.USD,
         to: Valute.BTC,
     }, {
-        from: Valute.BTC,
-        to: Valute.USD,
+        from: Valute.DSH,
+        to: Valute.NVC,
     }])).toEqual({
         btc_usd: value1,
         usd_btc: value2,
     });
+    expect(fetch.mock.calls).toEqual([["https://btc-e.com/api/3/ticker/usd_btc-dsh_nvc"]]);
+});
+it("depth", async () => {
+    const value = { test: 1 };
+    fetch.mockResponseOnce(JSON.stringify({ eth_nmc: value }));
+    const api = new PublicAPI();
+    expect(await api.depth(Valute.ETH, Valute.NMC)).toEqual(value);
+    expect(fetch.mock.calls).toEqual([["https://btc-e.com/api/3/depth/eth_nmc"]]);
+});
+it("trades", async () => {
+    const value = { test: 1 };
+    fetch.mockResponseOnce(JSON.stringify({ eth_dsh: value }));
+    const api = new PublicAPI();
+    expect(await api.trades(Valute.ETH, Valute.DSH)).toEqual(value);
+    expect(fetch.mock.calls).toEqual([["https://btc-e.com/api/3/trades/eth_dsh"]]);
 });
--- lib/typings.d.ts
+++ lib/typings.d.ts
@@ -2,4 +2,5 @@ declare namespace fetch {
     export function mockResponseOnce(body: string): void;
     export function mockReject(body: string): void;
     export function resetMocks(): void;
+    export const mock: jest.MockContext<any>;
 }
